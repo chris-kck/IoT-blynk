47248dd618ecdc5564b5498aef3f5096729033e3
Chris Kateera <kckateera@gmail.com>
2019-10-01 22:45:20 +0200

    Update enviro_logger.py

--- enviro_logger.py
+++ enviro_logger.py
@@ -15,12 +15,22 @@ import RPi.GPIO as GPIO
 import blynklib
 from time import time
 
+# Define values.
 BLYNK_AUTH = 'iPK7gVGF1PsbmT0de_TKOmmSUsIkSB50'
 blynk = blynklib.Blynk(BLYNK_AUTH)
-# Define values.
+btns = {sstop_btn:3,dismiss_btn:5, reset_time_btn:6, interval_btn:8} #dict of output pins
 
 def init_GPIO():
+    
+    #GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
+    GPIO.setup(btns, GPIO.IN, initial=0)    # set GPIO btns as inputs
+    GPIO.setup(in_pins, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Wire the button to +3.3V, then enable an internal pulldown. 
+    # GPIO.setup(x, GPIO.OUT) LED- https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
+    # GPIO.setup(x, GPIO.OUT) Buzzer
     b=3
+    #GPIO.add_event_detect(11, GPIO.BOTH, callback=count_up, bouncetime=200) #count up event detection
+    #GPIO.add_event_detect(13, GPIO.BOTH, callback=count_down, bouncetime=200) #count down event detection
     
 
 # ADC - Temperature, Potentiometer (Humidity), Light Sensor
@@ -35,7 +45,8 @@ def init_GPIO():
 
 # Main function
 def main():
-    a=4
+    #GPIO.output(12, GPIO.LOW)
+    
     print("hi")
     while 1:
         blynk.run()
@@ -48,3 +59,6 @@ if __name__ == "__main__":
         print("Exiting gracefully")
     except Exception as e:
         print("Error: {}".format(e))
+    finally:
+         GPIO.cleanup()
+        
